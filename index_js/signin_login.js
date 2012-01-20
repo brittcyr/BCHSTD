f09d4ed48e63f894d91e5be62faf4a484605a8cf
Tim Donegan <donegan@mit.edu>
2012-01-20 13:07:14 -0500

    Preparing Javascript for signup and login...save point, more changes will be made

---
+++ index_js/signin_login.js
@@ -0,0 +1,19 @@
+function viewSignup()
+{
+	document.getElementById("signup").style.display="block";
+}
+
+function viewLogin()
+{
+	document.getElementById("login").style.display="block";
+}
+
+function closeSignup()
+{
+	document.getElementById("signup").style.display="none";
+}
+
+function closeLogin()
+{
+	document.getElementById("login").style.display="none";
+}
