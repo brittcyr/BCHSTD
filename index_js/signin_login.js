a667be638259fce0d912ead2558e681d6d2dae38
Tim Donegan <donegan@mit.edu>
2012-01-23 11:59:44 -0500

    small_changes

--- index_js/signin_login.js
+++ index_js/signin_login.js
@@ -1,6 +1,6 @@
 var menu_open = false;
 
-
+//Opens menu for login or signup if no menu is open already
 function view(id)
 {
 	if (!(menu_open))
@@ -10,9 +10,18 @@ function view(id)
 	}
 }
 
+//Closes login/signup menu
 function hide(id)
 {
 	document.getElementById(id).style.display="none";
 	menu_open = false;
 }
 
+//confirm passwords are equivalent
+function passwordCheck(pass1, pass2)
+{
+	if (pass1 == pass2){
+		//code
+	}else{
+		//code
+	}
