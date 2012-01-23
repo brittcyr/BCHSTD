18d813c9d49847caed32723bdbb8b1f0e03aa338
Britt Cyr <cyrbritt@mit.edu>
2012-01-23 11:06:17 -0500

    modified:   candidates.html 	modified:   map.php

--- map.php
+++ map.php
@@ -11,9 +11,9 @@
 			</div>
 			<div id="navbar">
 				<a href="candidates.html"> Candidates </a>
-				<a href="home.html"> Home </a>
-				<a href="map.html"> Map </a>
-				<a href="index.html"> Logout </a>
+				<a href="home.php"> Home </a>
+				<a href="map.php"> Map </a>
+				<a href="logout.php"> Logout </a>
 			</div>		
 		</div>
 
