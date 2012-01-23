51fff49b970386536ce092e88f10cd8c5cdc4cf1
Britt Cyr <cyrbritt@mit.edu>
2012-01-23 13:07:47 -0500

    new file:   about_us.php 	new file:   leaderboard.php

---
+++ leaderboard.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>Leaderboard</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/menu_bar.css"/>
+	</head>
+
+	<body>
+		<div id="top">
+			<div id="logo">
+				<img src="icon_images/icon-usmap.png" />
+			</div>
+			<div id="navbar">
+				<a href="candidates.php"> Candidates </a>
+				<a href="home.php"> Home </a>
+				<a href="map.php"> Map </a>
+				<a href="logout.php"> Logout </a>
+			</div>		
+		</div>
+
+		<div id="main">
+			
+		</div>	
+
+	</body>
+</html>
