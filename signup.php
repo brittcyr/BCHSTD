5cb2e65086432deeed025b96b0dd2b8dac0b2eae
Britt Cyr <cyrbritt@mit.edu>
2012-01-24 09:48:53 -0500

    modified:   signup.php

--- signup.php
+++ signup.php
@@ -5,30 +5,37 @@ if (isset($_POST['user_email'])) {
 $email = htmlspecialchars($_POST['user_email']);
 $password = htmlspecialchars($_POST['user_password']);
 $confirm = htmlspecialchars($_POST['confirm_password']);
-        if ($password <> $confirm)
-        {header('Location:index.php');}
+
+if ($password <> $confirm)
+{header('Location:index.php');}
+
 $password = sha1($password);
 
 $email_split = explode('@',$email);
-$email = $email_split[0];
-if ( count($email_split) >  2){
+if ( count($email_split) <>  2){
 header('Location:index.php');
-}
+exit();}
 
+$name = $email_split[0];
+$domain = $email_split[1];
+
+$domain_split = explode('.',$domain);
+if ( count($domain_split) <>  2){
+header('Location:index.php');
+exit();}
 
 $query = "SELECT COUNT(*) FROM users WHERE email='$email'";
 $result = mysql_query($query) or die('bad query');
-        $result = mysql_fetch_array($result);
-        $result = $result[0];
+$result = mysql_fetch_array($result);
+$result = $result[0];
 
-if ($result>0) {
-                header('Location:index.php');
-} else {
-$query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
-$result = mysql_query($query) or die(mysql_error());
-                $_SESSION['user'] = $email;
-                header('Location:home.php');
-}
+if ($result>0) {header('Location:index.php');}
+else {
+       $query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+       $result = mysql_query($query) or die(mysql_error());
+       $_SESSION['user'] = $email;
+       header('Location:home.php');
+     }
 }
 mysql_close($db);
 ?>
