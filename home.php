ec139b93b0ae9f219c95349a8da6922d667a9b7b
Britt Cyr <cyrbritt@mit.edu>
2012-01-24 10:34:46 -0500

    modified:   home.php

--- home.php
+++ home.php
@@ -5,7 +5,6 @@ if (!isset($_SESSION['user'])){
    session_destroy();
    header('Location:index.php');
    exit();}
-echo 'Welcome, ' . $_SESSION['user'];
 ?>
 
 
@@ -26,6 +25,11 @@ echo 'Welcome, ' . $_SESSION['user'];
 				<a href="logout.php"> Logout </a>
 			</div>
 		</div>
+
+<?php
+echo 'Welcome, ' . $_SESSION['user'];
+?>
+
 		<div id="main">
 			<div id="news_feed">
 			  
@@ -33,9 +37,15 @@ echo 'Welcome, ' . $_SESSION['user'];
 			<div id="profile">
 				<h1> Profile </h1>
  <form class="forms" action="update_profile.php" method="post">
-	Email: <input type="text" name="user_email"/> <br/>
+
+<?php
+echo	'Email: <input id="user_email" type="text" name="user_email" value="'
+                . $_SESSION['user'] .'"/> <br/>';
+?>
 	Password: <input id="text1" type="password" name="user_password"/> <br/>
 	Confirm Password: <input id="text2" type="password" name="confirm_password"/> <br/>
+	Home State: <input id="home_state" type="text" name="home_state"/> <br/>
+	Political Party: <input id="party" type="text" name="party"/> <br/>
 	<input id="button1" type="submit" value="Submit!" />
       </form>
 			</div>
