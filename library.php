975b7695b714b8fdfdd377f6b37b8154af8923ab
Britt Cyr <cyrbritt@mit.edu>
2012-01-27 15:53:35 -0500

    modified:   library.php

--- library.php
+++ library.php
@@ -96,12 +96,17 @@ return $count;
 function gettotalplayers()
 {
 require_once 'db.php';
-$query = "SELECT COUNT(*) FROM user_selections GROUP BY email";
+$query = "SELECT * 
+	  FROM user_selections 
+	  GROUP BY email";
 $result = mysql_query($query) or die('bad query');
-$result = mysql_fetch_array($result);
-$result = $result[0];
+$count = 0;
+while ($row = mysql_fetch_array($result))
+{
+$count++;
+};
 mysql_close($db);
-return $result;
+return $count;
 }
 
 
@@ -168,13 +173,19 @@ mysql_close($db);
 /*function pull_popular_picks()
 {
 require_once 'db.php';
-$query = "SELECT INTO #temptable count(*) AS COUNT, state, candidate FROM user_selections GROUP BY state, candidate";
+$query = "SELECT INTO #temptable count(*) AS COUNT, state, candidate 
+	  FROM user_selections 
+	  GROUP BY state, candidate";
 $result = mysql_query($query) or die('bad query');
 query = "SELECT INTO #temptable2 MAX(COUNT) AS COUNT, state FROM #temptable GROUP BY state";
 $result = mysql_query($query) or die('bad query');
-query = "SELECT INTO #temptable3 A.state, A.candidate FROM #temptable2 AS A JOIN #temptable AS B  WHERE A.COUNT = B.COUNT";
+query = "SELECT INTO #temptable3 A.state, A.candidate 
+	 FROM #temptable2 AS A JOIN #temptable AS B
+	 ON A.COUNT = B.COUNT";
 $result = mysql_query($query) or die('bad query');
-query = "SELECT state, FIRST(candidate) FROM #temptable3 GROUP BY state";
+query = "SELECT state, FIRST(candidate) 
+	 FROM #temptable3 
+	 GROUP BY state";
 $result = mysql_query($query) or die('bad query');
 
 mysql_query("DROP TABLE #temptable, #temptable2, #temptable3");
