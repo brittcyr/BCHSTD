7879f68e7d281ac51137aa5301e9ff067550ad57
Sharon Hao <hao.sharon@gmail.com>
2012-01-29 15:23:48 -0500

    index

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="stylesheets/index_main.css"/>
 	<link rel="stylesheet" type="text/css" href="stylesheets/index_bottom.css"/>
 	<link rel="stylesheet" type="text/css" href="stylesheets/global.css"/>
+	<link rel="stylesheet" type="text/css" href="stylesheets/signup_login.css">
     <script type="text/javascript" src="javascript/signin_login.js"></script>
   </head>
   <body background="images/flag.jpg">
@@ -13,11 +14,13 @@
 	<img src="icon_images/icon-usmap.png"/>
       </div>
       <div id="NavBar">
-		      <form class="forms" action="login.php" method="post">
-	<input id="user_email" type="text" name="user_email" placeholder="Username or Email"/>
-	<input id="login_password" type="password" name="user_password" placeholder="Password"/>
-	<input id="button2" type="submit" value="Login" />
-      </form>
+		<div id="login">
+		<form class="forms" action="login.php" method="post">
+		<input id="user_email" type="text" name="user_email" placeholder="Username or Email"/>
+		<input id="login_password" type="password" name="user_password" placeholder="Password"/>
+		<input id="button2" type="submit" value="Login" />
+	    </form>
+		</div>
       </div>
     </div>
           <div id="main">
@@ -27,21 +30,26 @@
       </div>
       <div id="page_title">
 	<h1 id="title"> Fantasy Politics </h1> <br/>
-     	 <form class="forms" action="signup.php" method="post">
+     	 <form class="forms" id="signup_form" action="signup.php" method="post">
 		<div id="form_a"/>
-			Username: <br/>
-			Email: <br/>
-			Password: <br/>
-			Confirm Password: <br/>
+			Username <br/>
+			Email <br/>
+			Password <br/>
+			Confirm Password <br/>
 		</div>
 		<div id="form_b">
 			<input id="text5" type="text" name="username" onkeyup="checkSubmit()"/><br/> 
 			<input id="text3" type="text" name="user_email" onkeyup="checkSubmit()" /> <br/>
 			<input id="text1" type="password" name="user_password" onchange="checkSubmit()" onkeyup="checkSubmit()"/> <br/>
-			<input id="text2" type="password" name="confirm_password" onchange="checkSubmit()" onkeyup="checkSubmit()"/> <img id="x" src="images/x.png"/> <img id="check" src="images/check.png" style="display:none;"/> <br/>
+			<input id="text2" type="password" name="confirm_password" onchange="checkSubmit()" onkeyup="checkSubmit()"/><br/>
 		</div>
-	<div id="help_message">Reminder: The password must be at least six characters or numbers </div>
+		<div id='x_check'>
+		<img id="x" src="images/x.png"/> <img id="check" src="images/check.png" style="display:none;"/>
+		</div>
+	<span id="help_message">Reminder: The password must be at least six characters or numbers </span>
+	<div id="button1div">
 	<input id="button1" type="submit" value="Sign Up!!" />
+	</div>
       </form>
     </div>
     </div>
