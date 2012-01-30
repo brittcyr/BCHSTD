0a7e12de2ea2d706a037581a83498fd10faff64b
Sharon Hao <hao.sharon@gmail.com>
2012-01-30 15:27:56 -0500

    menu bar drop down

--- navbar.php
+++ navbar.php
@@ -3,18 +3,40 @@ session_start();
 if (isset($_SESSION['user']))
 {
 ?>
-
-<a href="logout.php"> Logout </a>
-<a href="FAQ.php">FAQ</a>
-<a href="about_us.php"> About Us </a>
-<a href="leaderboard.php"> Leaderboard </a>
-<a href="candidates.php"> Candidates </a>
-<a href="home.php"> Home </a>
-<a href="map.php"> Map </a>
-
+<ul id="nav">
+<li>
+        <a href="map.php">Map</a>
+</li>
+<li>
+        <a href="#">Statistics</a>
+        <ul>
+        <li><a href="home.php">My Stats</a></li>
+        <li><a href="leaderboard.php">Leaderboard</a></li>
+        </ul>
+</li>
+<li>
+        <a href="#">Candidates</a>
+        <ul>
+        <li><a href="http://www.mittromney.com">Mitt Romney</a></li>
+        <li><a href="http://www.ronpaul2012.com">Ron Paul</a></li>
+        <li><a href="http://www.ricksantorum.com">Rick Santorum></a></li>
+        <li><a href="http://www.newt.org">Newt Gingrich</a></li>
+        <li><a href="candidates.php">All</a></li>
+        </ul>
+</li>
+<li>
+        <a href="#">Account</a>
+        <ul>
+        <li><a href="FAQ.php">FAQ</a></li>
+        <li><a href="about_us.php">About Us</a></li>
+        <li><a href="logout.php">Logout</a></li>
+        </ul>
+</li>
+</ul>
 
 <?php
 }
 else{
-echo '<a href="index.php"> Back </a>';}
+echo '<ul id="nav"><li><a href="index.php"> Back </a><li></ul>';}
 ?>
+
