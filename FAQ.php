1abbbeb7f42e0e7a68e6730f61a663a8a67932cd
Sharon Hao <hsharon@mit.edu>
2012-01-31 15:44:01 -0500

    faq

--- FAQ.php
+++ FAQ.php
@@ -36,33 +36,34 @@ require_once 'checkcookies.php';
 				<a href="#why">Why was Fantasy Politics created?</a></br>
 			</div>
 			<div id="text">
-				<div class="q" name="what">
-					What is Fantasy Politics?
+				<div class="q">
+				<a name="what"></a>
+				<b>	What is Fantasy Politics?</b>
 				</div>
 				<div class="answer">
 					Fantasy Politics is a website built to encourage users to learn more about the upcoming presidential elections.
 				</div>
-				<div class="q" name="who">
-					Who is this site directed at?
+				<div class="q">
+				<a name="who"></a><b>Who is this site directed at?</b>
 				</div>
 				<div class="answer">
 					This site is directed at anyone with any political interest. This site is also directed at people that like to win.
 				</div>
-				<div class="q" name="win">
-					How do I increase my chances of winning?
+				<div class="q">
+				<a name="win"></a><b>How do I increase my chances of winning?</b>
 				</div>
 				<div class="answer">
 					The most ideal solution would be to look into the future and find out who wins each primary. But since we are only human, the next best solution would be to follow the debates going on in each state, learn about state demographics and voting patterns, and learn about each candidate's platform.
 				</div> 
 					
-				<div class="q" name="how">
-					How do I play?
+				<div class="q">
+				<a name="how"></a><b>How do I play?</b>
 				</div>
 				<div class="answer">
 					On the map page, when you are in 'choices' mode, you can select a candidate, and then click on a state to make your prediction. If the state has already held it's election, then you will not be allowed to make or change your choice.
 				</div>
-				<div class="q" name="why">
-					Why was Fantasy Politics created?
+				<div class="q">
+				<a name="why"></a><b>Why was Fantasy Politics created?</b>
 				</div>
 				<div class="answer">
 					Fantasy Politics was created for the MIT class 6.470. The goal of Fantasy Politics is to encourage an interest in politics. Most people are not very politically informed. We hope that when we throw a little competition into the mix, more people will be inspired to read up on candidates in order to make an educated selection.
