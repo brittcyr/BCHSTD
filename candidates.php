afdec73c8fab4aa91980eb69045cb3d4d3d5f935
Sharon Hao <hao.sharon@gmail.com>
2012-01-31 00:01:51 -0500

    candidates

--- candidates.php
+++ candidates.php
@@ -12,14 +12,18 @@
 	</head>
 
 	<body background="images/flag.jpg">
+		<script>
+			var el = document.getElementsByTagName("body")[0];
+			el.className = "";
+		</script>
 		<div id="top">
 		<div class="inner">
 			<div id="logo">
 				<h3> chooseyourchief.com </h3>
 			</div>
-			<div id="navbar">
+			<nav id="navbar">
 			  <?php require_once 'navbar.php'; ?>
-			</div>		
+			</nav>		
 		</div>
 		</div>
 
@@ -77,7 +81,27 @@
 				</p>
 			</div>
 		</div>	
+		<script src="javascript/jquery.js"></script>
+		<script src="javascript/modernizr.js"></script>
+		<script>
+			(function($){
+				var nav = $("#navbar");
 
+				nav.find("li").each(function() {
+					if ($(this).find("ul").length > 0) {
+						$("<span>").text("^").appendTo($(this).children(":first"));
+
+						$(this).mouseenter(function(){
+							$(this).find("ul").stop(true, true).slideDown(70);
+						});
+
+						$(this).mouseleave(function(){
+							$(this).find("ul").stop(true, true).slideUp(200);
+						});
+					}
+				});
+			})(jQuery);
+		</script>
 	</body>
 </html>
 
